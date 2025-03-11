dad6cf1929a8c308f06db4cdc5a5ebecaa2fd817
startleTurtle <startleTurt@gmail.com>
2025-03-11 19:45:00 +0900

    3/11 수업 내용 및 과제

---
+++ study/test/test/test.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+void main() {
+	printf("이름: 김형준\n");
+	printf("학번: 2025******\n");
+	printf("학교: 명지전문대학교\n");
+	printf("학과: 컴퓨터보안공학과\n");
+	printf("전화번호: 010-****-****\n");
+	printf("주소: 서울특별시\n");
+}
---
+++ study/test2/test2/test2.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+void main() {
+	printf("100 ¡¿ 100 = %d\n", 100 * 100);
+	printf("100 + 500 = %d\n", 100 + 500);
+	printf("100 - 50 = %d\n", 100 - 50);
+}
