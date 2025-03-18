450c37fce6c3cfbf372aca22e60b68265ed66c27
startleTurtle <startleTurt@gmail.com>
2025-03-18 12:08:40 +0900

    Add files via upload

--- exam2_2/exam2_2.c
+++ exam2_2/exam2_2.c
@@ -1,26 +1,42 @@
-#define _CRT_SECURE_NO_WARNINGS
+/*#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
 void main() {
-	int a, b;
+	int a, b, c;
 	int result;
 
 	printf("첫 번째 계산할 값을 입력하세요 ==> ");
 	scanf("%d", &a);
 	printf("두 번째 계산할 값을 입력하세요 ==> ");
 	scanf("%d", &b);
+	printf("세 번째 계산할 값을 입력하세요 ==> ");
+	scanf("%d", &c);
 
-	printf("다음의 계산기 프로그램을 하시오\n\n");
-
-	result = a + b;
-	printf("%d + %d = %d\n", a, b, result);	
+	result = a + b + c;
+	printf("\n%d + %d + %d = %d\n", a, b, c, result);	
 
 	result = a - b;
 	printf("%d - %d = %d\n", a, b, result);	
 	
-	result = a * b;
-	printf("%d * %d = %d\n", a, b, result);	
+	result = a * b * c;
+	printf("%d * %d * %d = %d\n", a, b, c, result);	
 	
 	result = a / b;
 	printf("%d / %d = %d\n", a, b, result);
+}*/
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+void main() {
+	int a, b, c;
+	int result;
+
+	printf("a, b, c 값을 입력하세요 ==> ");
+	scanf("%d %d %d", &a, &b, &c);
+
+	result = a + b + c;
+	printf("\n%d + %d + %d = %d\n", a, b, c, result);	
+
+	result = a * b * c;
+	printf("%d * %d * %d = %d\n", a, b, c, result);
 }
