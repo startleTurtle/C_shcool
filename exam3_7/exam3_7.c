ef3d3878fd43275507728202ad80ce3a9b987af4
startleTurtle <startleTurt@gmail.com>
2025-04-01 11:59:07 +0900

    Add files via upload

---
+++ exam3_7/exam3_7.c
@@ -0,0 +1,52 @@
+/*#include <stdio.h>
+void main() {
+	int a = 100, b = 200;
+	float result;
+
+	result = (float)a / b;
+
+	printf("%.1f \n", result);
+	
+}
+#include <stdio.h>
+void main() {
+	float a = 0.1234567980123456789012345f;
+	double b = 0.1234567890123456789012345;
+
+	printf("%30.25f \n", a);
+	printf("%30.25lf \n", b);
+}
+#include <stdio.h>
+void main() {
+	char a, b, c;
+
+	a = 'A';
+
+	printf("%c \n", a);
+	printf("%d \n", a);
+
+	b = 'a';
+	c = b + 5;
+	printf("%c \n", b);
+	printf("%c \n", c);
+
+	c = 90;
+	printf("%c \n", c);
+
+}
+#include <stdio.h>
+void main() {
+	int a, b;
+	char c, d;
+
+	a = 0x41;
+	b = 0x50;
+
+	printf("%c \n", b);
+
+	c = a;
+	printf("%c \n", c);
+
+	d = '#';
+	printf("%c의 ASCII 값은 %d 입니다. \n", d, d);
+}*/
