548f1fcff990cab06ae2becc6fb61160526cda8c
startleTurtle <startleTurt@gmail.com>
2025-05-20 11:20:14 +0900

    Update exam_12.c

--- exma_12/exam_12.c
+++ exma_12/exam_12.c
@@ -90,7 +90,7 @@ void main() {
 	printf("시작값, 끝값, 증가값 입력 : ");
 	scanf("%d %d %d", &num1, &num2, &num3);
 
-	for (i = num1; i < num2; i += num3) {
+	for (i = num1; i <= num2; i += num3) {
 		hap = hap + i;
 	}
 	printf("%d에서 %d까지 %d씩 증가한 값의 합: %d \n", num1, num2, num3, hap);
