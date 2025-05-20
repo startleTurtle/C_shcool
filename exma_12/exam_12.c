01112c224d76aca3f06d5b9804dba1804945b2ee
startleTurtle <startleTurt@gmail.com>
2025-05-20 10:40:43 +0900

    Add files via upload

---
+++ exma_12/exam_12.c
@@ -0,0 +1,55 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+void main() {
+	//int a, b;
+	//char ch;
+
+	/*
+	printf("첫 번째 수를 입력하세요 : ");
+	scanf("%d", &a);
+	printf("계산할 연산자를 입력하세요 : ");
+	scanf(" %c", &ch); //scanf_s(" %c", &ch, 1);
+	printf("두 번째 수를 입력하세요 : ");
+	scanf("%d", &b);
+
+	if (ch == '+') {
+		printf("%d + %d = %d 입니다. \n", a, b, a + b);
+	}
+	else if (ch == '-') {
+		printf("%d - %d = %d 입니다. \n", a, b, a - b);
+	}
+	else if (ch == '*') {
+		printf("%d * %d = %d 입니다. \n", a, b, a * b);
+	}
+	else if (ch == '/') {
+		printf("%d / %d = %f 입니다. \n", a, b, a / (float)b);
+	}
+	else if (ch == '%') {
+		printf("%d %% %d = %d 입니다. \n", a, b, a % b);
+	}
+	else {
+		printf("연산자를 잘못 입력했습니다. \n");
+	}*/
+	/*printf("수식을 한 줄로 띄어쓰기로 입력하세요 : ");
+	scanf("%d %c %d", &a, &ch, &b); //scnaf_s("%d %c %d", &a, &ch, 1, &b);
+
+	switch (ch) {
+	case '+':
+		printf("%d + %d = %d 입니다. \n", a, b, a + b);
+	case '-':
+		printf("%d - %d = %d 입니다. \n", a, b, a - b);
+		break;
+	case '*':
+		printf("%d * %d = %d 입니다. \n", a, b, a * b);
+		break;
+	case '/':
+		printf("%d + %d = %d 입니다. \n", a, b, a / b);
+		break;
+	case '%':
+		printf("%d %% %d = %d 입니다. \n", a, b, a % b);
+		break;
+	default:
+		printf("연산자를 잘못 입력했습니다. \n");
+	}*/
+}
