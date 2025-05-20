fe7cbd7a5101a279bca6ae486428752cda7afe9c
startleTurtle <startleTurt@gmail.com>
2025-05-20 11:19:14 +0900

    Update exam_12.c

--- exma_12/exam_12.c
+++ exma_12/exam_12.c
@@ -79,11 +79,20 @@ void main() {
 	*/
 
 	/*int hap = 0;
-	int i;
-
 	for (i = 3; i <= 100; i += 3) {
 		hap += i;
 	}
 	printf("1에서 100까지의 3의 배수의 합: %d", hap);
+	*/
+
+	/*int num1, num2, num3;
+
+	printf("시작값, 끝값, 증가값 입력 : ");
+	scanf("%d %d %d", &num1, &num2, &num3);
+
+	for (i = num1; i < num2; i += num3) {
+		hap = hap + i;
+	}
+	printf("%d에서 %d까지 %d씩 증가한 값의 합: %d \n", num1, num2, num3, hap);
 	*/
 }
